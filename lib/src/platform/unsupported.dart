f8698642b074c2a0ca21d9daffd2cbd8e16e3132
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2024-07-05 18:31:18 +0300

    fix(web): add web stubs

--- lib/src/platform/unsupported.dart
+++ lib/src/platform/unsupported.dart
@@ -8,27 +8,45 @@ class ReadDir {}
 
 class Metadata {}
 
-Metadata metadata(String path) =>
+Future<Metadata> metadata(String path) =>
     throw UnsupportedError("'metadata' is not supported on on this platform.");
+Metadata metadataSync(String path) => throw UnsupportedError(
+    "'metadataSync' is not supported on on this platform.");
 
-bool exists(String path) =>
+Future<bool> exists(String path) =>
     throw UnsupportedError("'exists' is not supported on on this platform.");
+bool existsSync(String path) => throw UnsupportedError(
+    "'existsSync' is not supported on on this platform.");
 
-bool isDir(String path) =>
+Future<bool> isDir(String path) =>
     throw UnsupportedError("'isDir' is not supported on on this platform.");
+bool isDirSync(String path) =>
+    throw UnsupportedError("'isDirSync' is not supported on on this platform.");
 
-bool isFile(String path) =>
+Future<bool> isFile(String path) =>
     throw UnsupportedError("'isFile' is not supported on on this platform.");
+bool isFileSync(String path) => throw UnsupportedError(
+    "'isFileSync' is not supported on on this platform.");
 
-bool isSymlink(String path) =>
+Future<bool> isSymlink(String path) =>
     throw UnsupportedError("'isSymlink' is not supported on on this platform.");
+bool isSymlinkSync(String path) => throw UnsupportedError(
+    "'isSymlinkSync' is not supported on on this platform.");
 
-Result<ReadDir, IoError> readDir(String path) =>
+Future<Result<ReadDir, IoError>> readDir(String path) =>
     throw UnsupportedError("'readDir' is not supported on on this platform.");
+Result<ReadDir, IoError> readDirSync(String path) => throw UnsupportedError(
+    "'readDirSync' is not supported on on this platform.");
 
-Result<String, IoError> readLink(String path) => throw UnimplementedError(
-    "'readLink' is not supported on on this platform.");
+Future<Result<String, IoError>> readLink(String path) =>
+    throw UnimplementedError(
+        "'readLink' is not supported on on this platform.");
+Result<String, IoError> readLinkSync(String path) => throw UnimplementedError(
+    "'readLinkSync' is not supported on on this platform.");
 
-Result<Metadata, IoError> symlinkMetadata(String path) =>
+Future<Result<Metadata, IoError>> symlinkMetadata(String path) =>
     throw UnimplementedError(
         "'symlinkMetadata' is not supported on on this platform.");
+Result<Metadata, IoError> symlinkMetadataSync(String path) =>
+    throw UnimplementedError(
+        "'symlinkMetadataSync' is not supported on on this platform.");
